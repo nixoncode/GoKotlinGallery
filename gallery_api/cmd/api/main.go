bc1e766b88dc4b9c1d4a2709ab9b06c7bed60ae8
Nixon Kosgei <nixon.kosgei@network.global>
2025-03-18 22:50:39 +0300

    implement main application logic with image upload, retrieval, and error handling

--- gallery_api/cmd/api/main.go
+++ gallery_api/cmd/api/main.go
@@ -1,1 +1,65 @@
 package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/labstack/echo/v4"
+	"github.com/nixoncode/gallery_api/internal/database"
+	"github.com/nixoncode/gallery_api/internal/handlers"
+	"github.com/nixoncode/gallery_api/internal/storage"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	e := echo.New()
+
+	// Middleware
+	//e.Use(middleware.Logger())
+	//e.Use(middleware.Recover())
+
+	// Error Handler
+	e.HTTPErrorHandler = customHTTPErrorHandler
+
+	baseDir := "uploads"
+	os.MkdirAll(baseDir, 0755)
+
+	db, err := database.ConnectDB()
+	if err != nil {
+		log.Fatalf("failed to connect to database: %v", err)
+	}
+
+	storage := storage.NewStorage(baseDir, db)
+	handlers := handlers.NewHandlers(storage, 10*1024*1024) // 10MB limit
+
+	e.POST("/upload", handlers.UploadImage)
+	e.GET("/image", handlers.GetImage)
+	e.GET("/details", handlers.GetImageDetails)
+
+	e.Logger.Fatal(e.Start("0.0.0.0:6661"))
+}
+
+type Response struct {
+	Message string      `json:"message,omitempty"`
+	Data    interface{} `json:"data,omitempty"`
+	Error   string      `json:"error,omitempty"`
+}
+
+func customHTTPErrorHandler(err error, c echo.Context) {
+	code := http.StatusInternalServerError
+	msg := "Internal Server Error"
+	if he, ok := err.(*echo.HTTPError); ok {
+		code = he.Code
+		msg = he.Message.(string)
+	}
+	if err := c.JSON(code, Response{Error: msg}); err != nil {
+		c.Logger().Error(err)
+	}
+	c.Logger().Error(err)
+}
