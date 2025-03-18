ef91a0d8c4115f883275ee5ef6aa341134867063
Nixon Kosgei <nixon.kosgei@network.global>
2025-03-18 21:31:43 +0300

    add migration for creating images table in the gallery database

---
+++ gallery_api/internal/database/migrations/20250318182858_create_images_table.up.sql
@@ -0,0 +1,9 @@
+CREATE TABLE gallery.images
+(
+    id SERIAL PRIMARY KEY,
+    filename VARCHAR(255) NOT NULL,
+    description TEXT,
+    metadata JSONB,
+    created_at TIMESTAMP
+    WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
